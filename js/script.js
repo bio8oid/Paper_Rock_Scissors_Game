9a81295568c86270235ef878f19867eb0df0b775
bio8oid <8bollod8@mail.co.uk>
2019-04-15 21:53:33 +0200

    Table generator update issue

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@
    newGameButton: document.getElementById('new-game'),
    randomNumber: 0,
    roundsNumber: 0,
-   total: 0,
+   total: 1,
    scoreX: 0,
    scoreY: 0,
    paper: 1,
@@ -47,14 +47,14 @@ function draw(){
 // Clear All Info \\
 
 function clear() {
-  params.progress = [];
   params.output.innerHTML= '';
   params.info.innerHTML= '';
   params.modalWon.innerHTML= '';
   params.scoreX = 0;
   params.scoreY = 0;
-  params.total = 0;
+  params.total = 1;
   params.result.innerHTML= '';
+  params.progress.map = [];
   params.progress = [];
 }
 
@@ -62,12 +62,11 @@ function clear() {
 
 function roundsCountdown(){
   if(params.roundsNumber == params.scoreX || params.roundsNumber == params.scoreY) {
-    
+
     params.output.innerHTML = '<br> Game over, please press the New Game button!';
     document.getElementById('won').classList.add('headPop');
     document.getElementById('trigger').click();
-    
-
+   
 
     params.ilusion.classList.add('magic');
     if(params.scoreX === params.scoreY) {
@@ -87,9 +86,9 @@ function roundsCountdown(){
 // Total Rounds Counter \\
 
 function total() {
-    
-    params.info.innerHTML = '<br>' + params.total++;
-  }
+
+  params.info.innerHTML = '<br>' + params.total++;
+}
 
 // Results Table Generator \\
 
@@ -100,6 +99,7 @@ function total() {
 
 function random() {
   params.randomNumber = Math.floor(Math.random() * 3) + 1;
+  
 }
 
 // Bio Player Move and Results Assess \\
@@ -109,10 +109,7 @@ function playerMove(event) {
   total();
 
 
-
   var attributeHandler = this.getAttribute("data-move");
-
-
 
   if(attributeHandler === "paper") {
    var value = 1;
@@ -126,212 +123,117 @@ function playerMove(event) {
 
  if(value === params.randomNumber) {
   params.output.innerHTML = '<br> DRAW';
-   draw();
-roundsCountdown();
-    
-    document.getElementById('trigger').click();
-    push();
- 
-   
+  draw();
+  roundsCountdown();  
+  document.getElementById('trigger').click();
 }
 if(value === 1 && params.randomNumber === 2) {
-    params.output.innerHTML = 'YOU - WON <br> Robo - Lost';
-    oneUpX();
-    roundsCountdown();
-    
-    document.getElementById('trigger').click();
-    push();
-
-     
-  }
-  if(value === 1 && params.randomNumber === 3) {
-    params.output.innerHTML =  'YOU - Lost <br> Robo - WON';
-    oneUpY();
-    roundsCountdown();
-    
-    document.getElementById('trigger').click();
-    push();
-
-     
-  }
-  if(value === 2 && params.randomNumber === 3) {
-    params.output.innerHTML = ' YOU - WON <br> Robo - Lost';
-    oneUpX();
-    roundsCountdown();
-    
-    document.getElementById('trigger').click();
-    push();
-
-     
-  }
-  if(value === 2 && params.randomNumber === 1) {
-    params.output.innerHTML = 'YOU - Lost <br> Robo - WON';
-    oneUpY();
-    roundsCountdown();
-    
-    document.getElementById('trigger').click();
-    push();
-
-     
-  }
-  if(value === 3 && params.randomNumber === 1) {
-    params.output.innerHTML = ' YOU - WON <br> Robo - Lost';
-    oneUpX();
-    roundsCountdown();
-    
-    document.getElementById('trigger').click();
-    push();
- 
-     
-  }
-  if(value === 3 && params.randomNumber === 2) {
-    params.output.innerHTML = 'YOU - Lost <br> Robo - WON';
-    oneUpY();
-    roundsCountdown();
-    
-    document.getElementById('trigger').click();
-    push();
-  }
-  console.log(params.progress);
-
-  function push() {
-params.progress.push(
-  {
-    "id": 'box1', 
-    "title": 'You', 
-    "content": attributeHandler, 
-    "categories": ""
-  },
-  {
-    "id": 'box2',
-    "title": 'Robo',
-    "content": params.randomNumber,
-    "categories": ""
-  },
-  {
-    "id": 'box3',
-    "title": 'Result',
-    "content": params.output.innerHTML,
-    "categories": ""
-  },
-  {
-    "id": 'box4',
-    "title": 'Score',
-    "content": params.result.innerHTML,
-    "categories": ""
-  },
-  {
-    "id": 'box5',
-    "title": 'Round',
-    "content": params.info.innerHTML,
-    "categories": ""
-  });
+  params.output.innerHTML = 'YOU - WON <br> Robo - Lost';
+  oneUpX();
+  roundsCountdown();
+  document.getElementById('trigger').click();
+  push();
+}
+if(value === 1 && params.randomNumber === 3) {
+  params.output.innerHTML =  'YOU - Lost <br> Robo - WON';
+  oneUpY();
+  roundsCountdown();
+  document.getElementById('trigger').click();
+  push();
+}
+if(value === 2 && params.randomNumber === 3) {
+  params.output.innerHTML = 'YOU - WON <br> Robo - Lost';
+  oneUpX();
+  roundsCountdown();
+  document.getElementById('trigger').click();
+  push();
+}
+if(value === 2 && params.randomNumber === 1) {
+  params.output.innerHTML = 'YOU - Lost <br> Robo - WON';
+  oneUpY();
+  roundsCountdown();
+  document.getElementById('trigger').click();
+  push();
+}
+if(value === 3 && params.randomNumber === 1) {
+  params.output.innerHTML = 'YOU - WON <br> Robo - Lost';
+  oneUpX();
+  roundsCountdown();
+  document.getElementById('trigger').click();
+  push();
+}
+if(value === 3 && params.randomNumber === 2) {
+  params.output.innerHTML = 'YOU - Lost <br> Robo - WON';
+  oneUpY();
+  roundsCountdown();
+  document.getElementById('trigger').click();
+  push();
 }
 
-function generate(x) { 
-  var div = document.createElement('div'); 
-  div.id = x.id;
-  div.classList.add('box', ...x.categories);
-  document.getElementById('table').appendChild(div);
- // header generate
-  var header = document.createElement('head');
-  var headerContent = document.createTextNode(x.title)
-  header.appendChild(headerContent);
-  document.getElementById(div.id).appendChild(header);
-   // paragraph generate
-  div.insertAdjacentHTML('beforeend', x.content);
-  
+
+console.log(params.progress);
+
+function push() {
+
+  if(params.randomNumber === 1 ) {
+   var Robo = "paper";
+ }
+ if(params.randomNumber === 2 ) {
+   var Robo = "rock";
+ }
+ if(params.randomNumber === 3 ) {
+   var Robo = "scisors";
+ }
+
+ params.progress.push(
+ {
+  "id": 'box1', 
+  "title": 'You', 
+  "content": '<p><br>' + attributeHandler + '</p>',
+},
+{
+  "id": 'box2',
+  "title": 'Robo',
+  "content": '<p><br>' + Robo + '</p>',
+},
+{
+  "id": 'box3',
+  "title": 'Result',
+  "content": '<p>' + params.output.innerHTML + '</p>',
+},
+{
+  "id": 'box4',
+  "title": 'Score',
+  "content":'<p>' + params.result.innerHTML + '</p>',
+},
+{
+  "id": 'box5',
+  "title": 'Round',
+  "content":'<p>' + params.info.innerHTML + '</p>',
+});
 }
 
- params.progress.map(generate);
-params.progress = [];
+
+  function generate(x) { 
+    var div = document.createElement('div'); 
+    div.id = x.id;
+    div.classList.add('box');
+    document.getElementById('table').appendChild(div);
+   // header generate
+   var header = document.createElement('head');
+   var headerContent = document.createTextNode(x.title)
+   header.appendChild(headerContent);
+   document.getElementById(div.id).appendChild(header);
+     // paragraph generate
+     div.insertAdjacentHTML('beforeend', x.content);
+     
+   };
+params.progress.map(generate);
 console.log(params.progress.map);
+params.progress.map = [];
+//params.progress = [];
+//console.log(params.progress.map);
 } 
-
-
-function eraseModal() {
-  var element = document.getElementById('table');
-    element.parentNode.removeChild(element);
-}
-function detach(node) {
-  var node = document.getElementById('table');
-  return node.parentElement.removeChild(node);
-}
-    
-/*
-  exampleOne['qwe'] = {
-
-  };
-exampleOne.push('Lorem ipsum');
-
-var progress = [];
-var box = {};
-box["id"] = 'box1';
-box["title"] = 'You';
-box["content"] = attributeHandler;
-params.progress.push(box);
-return progress;
-
-var nietos = [];
-nietos.push({"id": nieto.label, "02": nieto.value});
-return nietos;
-
-function push() {
-var box = {};
-box["id"] = 'box1';
-box["title"] = 'You';
-box["content"] = attributeHandler;
-params.progress.push(box);
-} 
-
-function push() {
-params.progress.push({"id": 'box1', "title": 'You', "content": attributeHandler});
-} 
-
-
-
- var data = [    
-  {
-    id: 'box1',
-    title: 'You',
-    content: attributeHandler,
-    categories: ['highlighted', 'special-header', 'important']
-  },
-  {
-    id: 'box2',
-    title: 'Robo',
-    content: '<p>Lorem !</p>',
-    categories: ['special-header', 'important']
-  },
-  {
-    id: 'box3',
-    title: 'Result',
-    content: '<p>Lorem !</p>',
-    categories: ['highlighted', 'important']
-  },
-  {
-    id: 'box4',
-    title: 'Score',
-    content: '<p>lorek</p>',
-    categories: ['highlighted']
-  },
-  {
-    id: 'box5',
-    title: 'Round',
-    content: '<p>Lorem !</p>',
-    categories: []
-  },
-];
-
-*/
-
-
-
-
-
-
-
-
 
 
 // Buttons \\
@@ -360,19 +262,18 @@ var showModal = function(event){
   document.querySelector('#modal-overlay').classList.add('show');
   var argValue = this.getAttribute('href');
   document.querySelector(argValue).classList.add('show');
-  }
-  var modalLinks = document.querySelectorAll('.show-modal');
-  for(var i = 0; i < modalLinks.length; i++){
-    modalLinks[i].addEventListener('click', showModal);
-  }
+}
+
+var modalLinks = document.querySelectorAll('.show-modal');
+for(var i = 0; i < modalLinks.length; i++){
+  modalLinks[i].addEventListener('click', showModal);
   
-  var hideModal = function(event){
-    event.preventDefault();
-    document.querySelector('#modal-overlay').classList.remove('show');
+}
 
-
-
-    detach();
+var hideModal = function(event){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
+    
   };
   
   var closeButtons = document.querySelectorAll('.modal .close');
